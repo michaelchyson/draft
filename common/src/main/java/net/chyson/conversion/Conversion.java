54772a3124267657fb2a543a382acb5409b73318
Michael Chyson <chyson@aliyun.com>
2018-06-20 18:21:38 +0800

    AES with hex test not ok: Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: 54 	at net.chyson.conversion.Conversion.byte2Hex(Conversion.java:44) 	at net.chyson.crypto.AESCipher.main(AESCipher.java:89)

--- common/src/main/java/net/chyson/conversion/Conversion.java
+++ common/src/main/java/net/chyson/conversion/Conversion.java
@@ -3,37 +3,37 @@ package net.chyson.conversion;
 
 public class Conversion {
 
-    public static String byte2HexComprehension(byte[] src) {
-        // byte: 8 bits
-        // short: 16 bits
-        // int: 32 bits
-        // long: 64 bits
-        // float: 32 bits
-        // double: 64 bits
-        StringBuilder stringBuilder = new StringBuilder();
-        if (src == null || src.length <= 0) {
-            return null;
-        }
+//    public static String byte2HexComprehension(byte[] src) {
+//        // byte: 8 bits
+//        // short: 16 bits
+//        // int: 32 bits
+//        // long: 64 bits
+//        // float: 32 bits
+//        // double: 64 bits
+//        StringBuilder stringBuilder = new StringBuilder();
+//        if (src == null || src.length <= 0) {
+//            return null;
+//        }
+//
+//        for (int i = 0; i < src.length; i++) {
+//            byte b = src[i];
+//            //return int by default, no effort to convert it to other type
+//            //besides this is toHexString method only in Integer.
+//            //to use the toHexString method
+//            int v = b & 0xFF; //return int be default
+//            String hv = Integer.toHexString(v);
+//
+//            stringBuilder.append(i + ":"); //for better understanding view
+//
+//            if (hv.length() < 2) {
+//                stringBuilder.append(0); //0x07 for example
+//            }
+//            stringBuilder.append(hv + ";");//for better understanding view
+//        }
+//        return stringBuilder.toString();
+//    }
 
-        for (int i = 0; i < src.length; i++) {
-            byte b = src[i];
-            //return int by default, no effort to convert it to other type
-            //besides this is toHexString method only in Integer.
-            //to use the toHexString method
-            int v = b & 0xFF; //return int be default
-            String hv = Integer.toHexString(v);
-
-            stringBuilder.append(i + ":"); //for better understanding view
-
-            if (hv.length() < 2) {
-                stringBuilder.append(0); //0x07 for example
-            }
-            stringBuilder.append(hv + ";");//for better understanding view
-        }
-        return stringBuilder.toString();
-    }
-
-    private static String byte2Hex(byte[] src) {
+    public static String byte2Hex(byte[] src) {
 
         StringBuilder stringBuilder = new StringBuilder();
         if (src == null || src.length <= 0) {
@@ -54,28 +54,28 @@ public class Conversion {
         return stringBuilder.toString();
     }
 
-    public static byte[] hexString2ByteComprehension(String hex) {
-        //hex string has 2 characters
-
-        hex = hex.toLowerCase();
-        int len = (hex.length() / 2);
-        System.out.println("length: " + hex.length());
-        System.out.println("len: " + len);
-
-        byte[] result = new byte[len];
-        System.out.println(hex);
-        char[] achar = hex.toCharArray();
-        for (char c : achar) {
-            System.out.print(c);
-            System.out.print("--");
-        }
-        System.out.println();
-        for (int i = 0; i < len; i++) {
-            int pos = i * 2;
-            result[i] = (byte) (char2Byte(achar[pos]) << 4 | char2Byte(achar[pos + 1]));
-        }
-        return result;
-    }
+//    public static byte[] hexString2ByteComprehension(String hex) {
+//        //hex string has 2 characters
+//
+//        hex = hex.toLowerCase();
+//        int len = (hex.length() / 2);
+//        System.out.println("length: " + hex.length());
+//        System.out.println("len: " + len);
+//
+//        byte[] result = new byte[len];
+//        System.out.println(hex);
+//        char[] achar = hex.toCharArray();
+//        for (char c : achar) {
+//            System.out.print(c);
+//            System.out.print("--");
+//        }
+//        System.out.println();
+//        for (int i = 0; i < len; i++) {
+//            int pos = i * 2;
+//            result[i] = (byte) (char2Byte(achar[pos]) << 4 | char2Byte(achar[pos + 1]));
+//        }
+//        return result;
+//    }
 
     public static byte[] hexString2Byte(String hex) {
         hex = hex.toLowerCase();
--- common/src/main/java/net/chyson/crypto/AESCipher.java
+++ common/src/main/java/net/chyson/crypto/AESCipher.java
@@ -8,8 +8,8 @@ import javax.crypto.spec.SecretKeySpec;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 
+import net.chyson.conversion.Conversion;
 import org.apache.commons.codec.binary.Base64;
-import org.codehaus.jackson.map.Serializers;
 
 /**
  * michael.chyson
@@ -22,10 +22,18 @@ public class AESCipher {
 
     private SecretKeySpec key;
 
+    private byte[] bytesKey;
+
     public AESCipher(String key) {
         byte[] decode = new Base64().decode(key);
 
         this.key = new SecretKeySpec(decode, ALGORITHM);
+    }
+
+    public AESCipher(byte[] bytesKey) {
+        this.bytesKey = bytesKey;
+        this.key = new SecretKeySpec(bytesKey, ALGORITHM);
+
     }
 
     public String encryt(String data) throws InvalidKeyException, BadPaddingException, IllegalBlockSizeException, NoSuchPaddingException, NoSuchAlgorithmException {
@@ -46,40 +54,91 @@ public class AESCipher {
         byte[] decode = new Base64().decode(data);
         byte[] bytes = cipher.doFinal(decode);
         return new String(bytes);
+    }
 
+    public String encrytHex(String data) throws InvalidKeyException, BadPaddingException, IllegalBlockSizeException, NoSuchPaddingException, NoSuchAlgorithmException {
+        Cipher cipher = Cipher.getInstance(TRANSFORMATION);
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        byte[] bytes = cipher.doFinal(data.getBytes());
+        //Encodes a byte[] containing binary data, into a String containing characters in the Base-N alphabet.
+        //Uses UTF8 encoding.
+        String s = Conversion.byte2Hex(bytes);
+        return s;
+    }
+
+    public String decryptHex(String data) throws InvalidKeyException, BadPaddingException, IllegalBlockSizeException, NoSuchPaddingException, NoSuchAlgorithmException {
+        Cipher cipher = Cipher.getInstance(TRANSFORMATION);
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        // Decodes a String containing characters in the Base-N alphabet into binary data.
+        byte[] decode = Conversion.hexString2Byte(data);
+        byte[] bytes = cipher.doFinal(decode);
+        return new String(bytes);
     }
 
 
     public static void main(String[] args) throws Exception {
-        String KEY = "oKHzBrHOUzUh2sziSAtOvg==";
-        String WRONG_KEY = "Michael Ming Chyson";
-        System.out.println(KEY.length());
-        System.out.println(WRONG_KEY.length());
+        String KEY = "oKHzBrHOUzUh2sziSAtOvg==";//length:24
+
         byte[] decode = new Base64().decode(KEY);
         SecretKeySpec key = new SecretKeySpec(decode, ALGORITHM);
         Cipher cipher = Cipher.getInstance(TRANSFORMATION);
         cipher.init(Cipher.ENCRYPT_MODE, key);
         byte[] hello_worlds = cipher.doFinal("hello world".getBytes());
-        String encryptedString = new Base64().encodeToString(hello_worlds);
+
+
+        String encryptedString = Conversion.byte2Hex(hello_worlds);
         System.out.println(encryptedString);
 
 
-        cipher.init(Cipher.DECRYPT_MODE, key);
-        byte[] decode1 = new Base64().decode(encryptedString);
-        byte[] bytes = cipher.doFinal(decode1);
-        String s = new String(bytes);
-        String s1 = new Base64().encodeToString(bytes);
-
-        System.out.println("s:" + s);
-        System.out.println("s1:" + s1);
-
-
-        AESCipher aesCipher = new AESCipher(KEY);
-        String michael_chyson = aesCipher.encryt("michael chyson");
-        System.out.println(michael_chyson);
-
-        String decrypt = aesCipher.decrypt(michael_chyson);
-        System.out.println(decrypt);
+//        cipher.init(Cipher.DECRYPT_MODE, key);
+//        byte[] decode1 = new Base64().decode(encryptedString);
+//        byte[] bytes = cipher.doFinal(decode1);
+//        String s = new String(bytes);
+//        String s1 = new Base64().encodeToString(bytes);
+//
+//        System.out.println("s:" + s);
+//        System.out.println("s1:" + s1);
+//
+//
+//        AESCipher aesCipher = new AESCipher(KEY);
+//        String michael_chyson = aesCipher.encryt("michael chyson");
+//        System.out.println(michael_chyson);
+//
+//        String decrypt = aesCipher.decrypt(michael_chyson);
+//        System.out.println(decrypt);
 
     }
+
+//    public static void main(String[] args) throws Exception {
+//        String KEY = "oKHzBrHOUzUh2sziSAtOvg==";
+//        String WRONG_KEY = "Michael Ming Chyson";
+//        System.out.println(KEY.length());
+//        System.out.println(WRONG_KEY.length());
+//        byte[] decode = new Base64().decode(KEY);
+//        SecretKeySpec key = new SecretKeySpec(decode, ALGORITHM);
+//        Cipher cipher = Cipher.getInstance(TRANSFORMATION);
+//        cipher.init(Cipher.ENCRYPT_MODE, key);
+//        byte[] hello_worlds = cipher.doFinal("hello world".getBytes());
+//        String encryptedString = new Base64().encodeToString(hello_worlds);
+//        System.out.println(encryptedString);
+//
+//
+//        cipher.init(Cipher.DECRYPT_MODE, key);
+//        byte[] decode1 = new Base64().decode(encryptedString);
+//        byte[] bytes = cipher.doFinal(decode1);
+//        String s = new String(bytes);
+//        String s1 = new Base64().encodeToString(bytes);
+//
+//        System.out.println("s:" + s);
+//        System.out.println("s1:" + s1);
+//
+//
+//        AESCipher aesCipher = new AESCipher(KEY);
+//        String michael_chyson = aesCipher.encryt("michael chyson");
+//        System.out.println(michael_chyson);
+//
+//        String decrypt = aesCipher.decrypt(michael_chyson);
+//        System.out.println(decrypt);
+//
+//    }
 }
